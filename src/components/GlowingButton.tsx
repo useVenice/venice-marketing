962f22bd0cb51d6767ed2174b5571dbe9ce28aa7
Rob Phillips <rob@robphillips.me>
2023-01-16 20:59:37 -0500

    [CSS] Fix issue with tailwind not rendering partially named classes
    See https://stackoverflow.com/a/74959709/308315

--- src/components/GlowingButton.tsx
+++ src/components/GlowingButton.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { twMerge } from 'tailwind-merge'
 
 export type GlowingButtonProps = {
   link?: any;
@@ -11,15 +10,15 @@ export type GlowingButtonProps = {
 }
 
 const defaultProps = {
-  color: "venice-green",
-  glowColor: "venice-green-glow"
+  color: "bg-venice-green",
+  glowColor: "shadow-venice-green-glow"
 }
 
 export function GlowingButton(props: GlowingButtonProps) {
   props = { ...defaultProps, ...props }
   return (
     <Link href={props.link} className={props.className}>
-      <button className={twMerge("bg-" + props.color, "shadow-lg shadow-" + props.glowColor, "flex py-2 pr-2 pl-4 border border-venice-inner-bevel rounded-lg")}> 
+      <button className={`flex py-2 pr-2 pl-4 border border-venice-inner-bevel rounded-lg ${props.color} shadow-lg ${props.glowColor}`}> 
         <span className="mt-1 font-sans text-[13px] text-venice-white">{props.text}</span>
         <Image src='/assets/arrow-right-white.svg' alt='Right arrow' width={27} height={27} className="ml-2" />
       </button>
