a0fd0c42e1a5da304c7254b1217c6c4f12011682
Rob Phillips <rob@robphillips.me>
2023-01-18 13:49:44 -0500

    [SEO] Tweaks to try and make Facebook's linter happy

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -22,9 +22,13 @@ class VeniceDocument extends Document {
           <meta property="og:title" content="Venice — Financial data, fast." />
           <meta property="og:type" content="website" />
           <meta property="og:url" content="https://venice.is" />
-          <meta property="og:image" content="https://venice.is/assets/og-image.png" />
+          <meta property="og:image" content="https://www.venice.is/assets/og-image.png" />
+          <meta property="og:image:width" content="2400" />
+          <meta property="og:image:height" content="1260" />
+          <meta property="og:image:alt" content="Venice is the open-source data infrastructure trusted by data analysts, personal finance geeks, and developers to unify & access their finances in the most powerful & flexible ways." />
+          <meta property="og:image:type" content="image/png" />
           <meta property="og:description" content="Venice is your financial database. Continuously sync data from over 10,000 financial institutions & multiple data providers in minutes, saving yourself hours of manual input & expensive copy/paste mistakes." />
-          <meta property="og:site_name" content="My Website" />
+          <meta property="og:site_name" content="Venice" />
 
           <Script src="https://tally.so/widgets/embed.js" strategy="beforeInteractive"></Script>
         </Head>
