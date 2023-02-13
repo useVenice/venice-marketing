0f865036d172c2000f0b3215902c4b0c7d637140
Rob Phillips <rob@robphillips.me>
2023-02-12 23:39:51 -0800

    [SEO] Open graph update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,27 +16,28 @@ class VeniceDocument extends Document {
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="manifest" href="/site.webmanifest" />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#12B886" />
+          <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
           <meta name="msapplication-TileColor" content="#12B886" />
           <meta name="theme-color" content="#12B886" />
           <meta
             name="description"
-            content="Venice is your financial database. Continuously sync data from over 12,000 financial institutions & multiple data providers in minutes, saving yourself hours of manual input & expensive copy/paste mistakes."
+            content="Get a fully hosted Plaid integration in minutes. Let us handle the heavy lifting so you can focus on building the future faster. Your customers will thank you."
           />
           <meta property="og:title" content="Venice — Frictionless financial data for developers." />
           <meta property="og:type" content="website" />
           <meta property="og:url" content="https://venice.is" />
           <meta property="og:image" content="http://venice-finance.s3.us-west-1.amazonaws.com/og-image.png" />
           <meta property="og:image:secure_url" content="https://venice-finance.s3.us-west-1.amazonaws.com/og-image.png" />
-          <meta property="og:image:width" content="2400" />
-          <meta property="og:image:height" content="1260" />
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="1200" />
           <meta
             property="og:image:alt"
-            content="Venice is the open-source data infrastructure trusted by data analysts, personal finance geeks, and developers to unify & access their finances in the most powerful & flexible ways."
+            content="Get a fully hosted Plaid integration in minutes. Let us handle the heavy lifting so you can focus on building the future faster. Your customers will thank you."
           />
           <meta property="og:image:type" content="image/png" />
           <meta
             property="og:description"
-            content="Venice is your financial database. Continuously sync data from over 12,000 financial institutions & multiple data providers in minutes, saving yourself hours of manual input & expensive copy/paste mistakes."
+            content="Get a fully hosted Plaid integration in minutes. Let us handle the heavy lifting so you can focus on building the future faster. Your customers will thank you."
           />
           <meta property="og:site_name" content="Venice" />
 
